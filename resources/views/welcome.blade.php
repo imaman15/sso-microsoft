a445dcf5d6216317eb0adbbb04a462825d5af492
imaman15 <imamagustiannugraha@ymail.com>
2021-10-13 12:09:03 +0700

    Add Calendar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     @if(isset($userName))
     <h4>Welcome {{ $userName }}!</h4>
     <p>Use the navigation bar at the top of the page to get started.</p>
+    <p>{{ $userTimeZone }}</p>
     @else
     <a href="/signin" class="btn btn-primary btn-large">Click here to sign in</a>
     @endif
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CalendarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,6 @@ Route::get('/', [HomeController::class, 'welcome']);
 Route::get('/signin', [AuthController::class, 'signin']);
 Route::get('/callback', [AuthController::class, 'callback']);
 Route::get('/signout', [AuthController::class, 'signout']);
+Route::get('/calendar', [CalendarController::class, 'calendar']);
+Route::get('/calendar/new', [CalendarController::class, 'getNewEventForm']);
+Route::post('/calendar/new', [CalendarController::class, 'createNewEvent'])->name("calendarnew");
