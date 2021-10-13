93e63284557f120b4d8299c6caa93e0f02912a15
imaman15 <imamagustiannugraha@ymail.com>
2021-10-13 09:53:57 +0700

    Refreshing tokens

--- app/TokenStore/TokenCache.php
+++ app/TokenStore/TokenCache.php
@@ -39,6 +39,48 @@ class TokenCache
             return '';
         }
 
+        // Check if token is expired
+        //Get current time + 5 minutes (to allow for time differences)
+        $now = time() + 300;
+        if (session('tokenExpires') <= $now) {
+            // Token is expired (or very close to it)
+            // so let's refresh
+
+            // Initialize the OAuth client
+            $oauthClient = new \League\OAuth2\Client\Provider\GenericProvider([
+                'clientId'                => config('azure.appId'),
+                'clientSecret'            => config('azure.appSecret'),
+                'redirectUri'             => config('azure.redirectUri'),
+                'urlAuthorize'            => config('azure.authority') . config('azure.authorizeEndpoint'),
+                'urlAccessToken'          => config('azure.authority') . config('azure.tokenEndpoint'),
+                'urlResourceOwnerDetails' => '',
+                'scopes'                  => config('azure.scopes')
+            ]);
+
+            try {
+                $newToken = $oauthClient->getAccessToken('refresh_token', [
+                    'refresh_token' => session('refreshToken')
+                ]);
+
+                // Store the new values
+                $this->updateTokens($newToken);
+
+                return $newToken->getToken();
+            } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+                return '';
+            }
+        }
+
+        // Token is still valid, just return it
         return session('accessToken');
+    }
+
+    public function updateTokens($accessToken)
+    {
+        session([
+            'accessToken' => $accessToken->getToken(),
+            'refreshToken' => $accessToken->getRefreshToken(),
+            'tokenExpires' => $accessToken->getExpires()
+        ]);
     }
 }
